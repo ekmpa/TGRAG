68426cfab9a626cdf5a8364d3f45f63fd8c9ad17
ekmpa <emmakndp@gmail.com>
2025-06-21 05:30:27 -0400

    Adding utils for WET work

--- tgrag/utils/path.py
+++ tgrag/utils/path.py
@@ -1,4 +1,6 @@
+import os
 import pathlib
+from glob import glob
 
 
 def get_root_dir() -> pathlib.Path:
@@ -7,3 +9,22 @@ def get_root_dir() -> pathlib.Path:
 
 def get_cwd() -> pathlib.Path:
     return pathlib.Path.cwd()
+
+
+def get_crawl_data_path(project_dir: str) -> str:
+    return os.path.join(
+        os.environ.get('SCRATCH', os.path.join(project_dir, 'data')), 'crawl-data'
+    )
+
+
+def get_wet_file_path(slice_id: str, project_dir: str) -> str:
+    scratch = os.environ.get('SCRATCH', project_dir)
+    base_path = os.path.join(scratch, 'crawl-data', slice_id, 'segments')
+    segment_dirs = glob(os.path.join(base_path, '*', 'wet', '*.warc.wet.gz'))
+
+    if not segment_dirs:
+        raise FileNotFoundError(
+            f'No WET file found for slice {slice_id} in {base_path}'
+        )
+
+    return segment_dirs[0]
