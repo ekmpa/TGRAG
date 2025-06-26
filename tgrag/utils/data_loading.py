d5790b32294046504110478148ce7023aea8199c
sebastian9991 <sebastian.sabry1@gmail.com>
2025-06-25 23:29:56 -0400

    adding option on paramater type for data_loading

--- tgrag/utils/data_loading.py
+++ tgrag/utils/data_loading.py
@@ -6,7 +6,7 @@ from torch import Tensor
 
 
 def load_node_csv(
-    path: str, index_col: int, encoders: Dict
+    path: str, index_col: int, encoders: Dict | None = None
 ) -> Tuple[Tensor | None, Dict]:
     df = pd.read_csv(path, index_col=index_col)
     mapping = {index: i for i, index in enumerate(df.index.unique())}
@@ -30,7 +30,7 @@ def load_edge_csv(
     path: str,
     src_index_col: str,
     dst_index_col: str,
-    encoders: Dict,
+    encoders: Dict | None = None,
 ) -> Tuple[Tensor, Tensor | None]:
     df = pd.read_csv(path)
 
